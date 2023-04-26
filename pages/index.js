147e9c50ad7903488e5103eaa9e57c33fb0002b3
Aja Singer <ajasinger@gmail.com>
2023-04-26 12:43:50 -0400

    add basic function to each js file

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
 // domain.com
+// contains React components that will load with that page 
+
+function HomePage() {
+    return <h1>The home page</h1>
+}
+
+export default HomePage;
--- pages/news.js
+++ pages/news.js
@@ -1,1 +1,7 @@
 //domain.com/news
+
+function NewsPage() {
+    return <h1>The news page</h1>
+}
+
+export default NewsPage;

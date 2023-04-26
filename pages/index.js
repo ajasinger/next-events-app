0839a6472d0178967ae87ae4c61e783959dd92ef
Aja Singer <ajasinger@gmail.com>
2023-04-26 14:47:15 -0400

    return MeetupList ocmponent in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 // domain.com
 // contains React components that will load with that page 
 
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        name: 'A First Meetup',
+        image: '',
+        address: 'address 1',
+        description: 'this is the first meetup'
+    },
+    {
+        id: 'm2',
+        name: 'A Second Meetup',
+        image: '',
+        address: 'address 2',
+        description: 'this is the second meetup'
+    }
+]
+
 function HomePage() {
-    return <h1>The home page</h1>
+    return (
+        < MeetupList meetups={DUMMY_MEETUPS} />
+    )
 }
 
 export default HomePage;
